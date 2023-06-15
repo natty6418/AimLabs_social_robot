b5eaf29e0ef3a6362ea34c6a2588acf510888377
natty6418 <>
2023-06-15 16:11:32 +0200

    Made a visual timer, fixed the problem with false postive results, implemented custom audio.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,63 @@
-import React from 'react';
-import {useState} from 'react'
-import {useRef} from 'react'
-import './App.css';
-import Header from './Components/Header'
-import Main from './Components/Main'
+import React from "react";
+import { useState } from "react";
+import { useRef } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
 export default function App() {
-  const [trial, setTrial] = React.useState(1)
+  const [trial, setTrial] = React.useState(0);
   const [time, setTime] = useState(Date.now());
-  const count = useRef(0);
-  const [wrong, setWrong] = useState(false)
-  const [responseTime, setResponseTime] = useState({0: {'Time':Date.now(), 'Wrong': false}});
-  const experiment_no = 0
-  function ResponseTime(){
+  const [wrong, setWrong] = useState(false);
+  const [responseTime, setResponseTime] = useState({
+    0: { Time: Date.now(), Wrong: false },
+  });
+  const experiment_no = 0;
+  function ResponseTime() {
     const currentTime = Date.now();
-    count.current +=1;
-    setResponseTime(prevResponseTime => ({
-      ...prevResponseTime, 
-      [count.current]: {
-      'Time': Math.floor((currentTime - time)),
-      'Wrong': wrong
-      }
+    setTrial((prev) => prev + 1);
+    setResponseTime((prevResponseTime) => ({
+      ...prevResponseTime,
+      [trial]: {
+        Time: Math.floor(currentTime - time),
+        Wrong: wrong,
+      },
     }));
-    setTime(Date.now())
-      }
+    setTime(Date.now());
+  }
   function incrementTrial() {
-    setTrial(prev => prev+1)
+    setTrial((prev) => prev + 1);
   }
   function download() {
     const link = document.createElement("a");
-    const file = new Blob([JSON.stringify(responseTime)], { type: 'text/plain' });
+    const file = new Blob([JSON.stringify(responseTime)], {
+      type: "text/plain",
+    });
     link.href = URL.createObjectURL(file);
     link.download = `experiment ${experiment_no}`;
     link.click();
     URL.revokeObjectURL(link.href);
-    
   }
-  function handleWrong(w){
-    setWrong(w)
+  function handleWrong(w) {
+    setWrong(w);
   }
   return (
-    <div className='app'>
-      <Header trialNum={trial} />
-      {trial < 50 ? <Main handleChange = {incrementTrial} responseTime = {responseTime} ResponseTime = {ResponseTime} handleWrong = {handleWrong}/> : 
-      (
-        <div className='end-trail'>
+    <div className="app">
+      <Header trialNum={trial}/>
+      {trial <= 20 ? (
+        <Main
+          setTime={setTime}
+          responseTime={responseTime}
+          ResponseTime={ResponseTime}
+          handleWrong={handleWrong}
+        />
+      ) : (
+        <div className="end-trail">
           <h1>End of Trial.</h1>
-          <button className='download-btn' onClick={()=>download()}>Download</button>
+          <button className="download-btn" onClick={() => download()}>
+            Download
+          </button>
         </div>
-        )}
+      )}
     </div>
-  )
-};
+  );
+}
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,95 +1,116 @@
-import React from 'react';
-import {useState} from 'react';
-import { useEffect } from 'react';
-import {useRef} from 'react';
-import play from '../Images/play.png' 
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+import play from "../Images/play.png";
 // import QTrobot from './js/qtrobot-1.0.min.js';
-export default function Main(props){
-  const [start, setStart] = useState(false)
-  const [url, setUrl] = useState(null)
-  const[qtrobot, setQtrobot] = useState({})
-  const shapes = ["Circle","Rectangle","Triangle","Diamond"]
-  const target = "Triangle"
-  let randomElt= ""
-  function RandomSymbol(){
-    var symbols = ["1", "2","3","4"];
-    var rand_num = Math.floor(Math.random() * ((symbols.length-1) - 0 + 1)) + 0;
+export default function Main(props) {
+  const [start, setStart] = useState(false);
+  const [url, setUrl] = useState(null);
+  const [qtrobot, setQtrobot] = useState({});
+  const [timerWidth, setTimerWidth] = useState(400);
+  const [randomShape, setRandomShape] = useState(<div className={"Circle"}></div>)
+  const shapes = ["Circle", "Rectangle", "Triangle", "Diamond"];
+  const target = "Triangle";
+  const randomElt = useRef("")
+  function RandomSymbol() {
+    var symbols = ["1", "2", "3", "4"];
+    var rand_num = Math.floor(Math.random() * (symbols.length - 1 - 0 + 1)) + 0;
     return symbols[rand_num];
   }
-  
-  useEffect(
-    () => {
-      var _url = prompt("Please enter QTrobot rosbridge url:", "ws://192.168.100.2:9091");
-      _url = (_url == null) ? 'ws://127.0.0.1:9091' : _url;
-      if (url !== _url){
-        setUrl(_url)
-      }
-      console.log("connecting to QTrobot (please wait...)");
-      
-    }, []
-  )
-  useEffect(()=>{
-    // eslint-disable-next-line no-undef
-    setQtrobot(()=> new QTrobot({
-      url : url,
-      connection: function(){    
-          console.log("connected to " + url);        
-      },
-      error: function(error){
-          console.log(error);
-      },
-      close: function(){
-          console.log("disconnected.");
-      }
-    }))
-  }, [url])
+
   useEffect(() => {
-    if(start) {
+    var _url = prompt(
+      "Please enter QTrobot rosbridge url:",
+      "ws://192.168.100.2:9091"
+    );
+    _url = _url == null ? "ws://127.0.0.1:9091" : _url;
+    if (url !== _url) {
+      setUrl(_url);
+    }
+    console.log("connecting to QTrobot (please wait...)");
+  }, []);
+  useEffect(() => {
+    // eslint-disable-next-line no-undef
+    setQtrobot(() => new QTrobot({
+          url: url,
+          connection: function () {
+            console.log("connected to " + url);
+          },
+          error: function (error) {
+            console.log(error);
+          },
+          close: function () {
+            console.log("disconnected.");
+          },
+        })
+    );
+  }, [url]);
+  useEffect(() => {
+    if (start) {
       const interval = setInterval(() => {
+        setRandomShape(()=>RandomShape())
         props.ResponseTime();
         // props.handleChange();
-        target === randomElt ? props.handleWrong(true) : props.handleWrong(false);
+        target === randomElt
+          ? props.handleWrong(true)
+          : props.handleWrong(false);
+        setTimerWidth(400)
       }, 1000);
-      return () => clearInterval(interval);
-    } 
-  },[props.responseTime])
+      const visual_interval = setInterval(() => {
+        setTimerWidth(prev => (prev- 4));
+      }, 10)
+      return () => {
+        clearInterval(interval);
+        clearInterval(visual_interval);
+      };
+    }
+  }, [props.responseTime]);
   function handleClick() {
-    qtrobot.set_volume(50)
-    qtrobot.call_service('/qt_robot/audio/play', '/qt_robot_interface/audio_play',{"filename": 'sample', "filepath": '../audio/'})
+    qtrobot.set_volume(20);
+    
+    // qtrobot.call_service(
+    //   "/qt_robot/audio/play",
+    //   "/qt_robot_interface/audio_play",
+    //   { filename: "sample", filepath: "~/robot/data/audios/sample.mp3" }
+    // );
     // qtrobot.home_motors()
     // qtrobot.show_emotion('QT/with_a_cold_sneezing', qtrobot.talk_text('achoo'));
-    // qtrobot.play_audio('sample', '/home/qtrobot/AimLabs_social_robot/src/audio')
+    qtrobot.play_audio('sample')
     props.ResponseTime();
     // props.handleChange();
-    target === randomElt ? props.handleWrong(false) : props.handleWrong(true);
+    target === randomElt.current ? (
+      props.handleWrong(false)) : props.handleWrong(true);
+    setRandomShape(()=>RandomShape());
+    setTimerWidth(400)
   }
-  function RandomShape(){
-    randomElt = shapes[Math.floor(Math.random() * ((shapes.length-1) - 0 + 1))]
-    return(
-      <div className={randomElt}></div>
-    )
+  function RandomShape() {
+    randomElt.current = shapes[Math.floor(Math.random() * (shapes.length - 1 - 0 + 1))];
+    return <div className={randomElt.current}></div>;
   }
-  function handleStart(){
+  function handleStart() {
     // console.log("dhdjkhd")
     setStart(true);
     props.setTime(Date.now());
     props.ResponseTime();
   }
-  return (
-    !start ? <img className='play-btn' onClick={()=>handleStart()} src={play}/> :
-    <div className='main'>
+  return !start ? (
+    <img className="play-btn" onClick={() => handleStart()} src={play} />
+  ) : (
+    <div className="main">
       <div className="prompt">
-        {RandomShape()}
-          {/* {props.numbers && <p>{RandomSymbol()}</p>}
+        {randomShape}
+        {/* {props.numbers && <p>{RandomSymbol()}</p>}
           {props.shapes && RandomShape()} */}
       </div>
-      <div className="response" onClick={()=>handleClick()}>
-          <span className="checkmark">
-            {/* <div className="checkmark_circle"></div> */}
-            <div className="checkmark_stem"></div>
-            <div className="checkmark_kick"></div>
-          </span>
+      <div className="visual-time" style={{'width': `${timerWidth}px`}}></div>
+      <div className="response" onClick={() => handleClick()}>
+        <span className="checkmark">
+          {/* <div className="checkmark_circle"></div> */}
+          <div className="checkmark_stem"></div>
+          <div className="checkmark_kick"></div>
+        </span>
       </div>
     </div>
-  )
+  );
 }
